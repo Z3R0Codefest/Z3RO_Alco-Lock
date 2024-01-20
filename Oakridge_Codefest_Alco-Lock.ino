6169fa8a55c2fe0e2ab0d18a17b0e500c7e6a0df
Z3R0_Hackathons <123229942+Z3R0Codefest@users.noreply.github.com>
2024-01-20 13:49:31 +0530

    Add files via upload

--- Oakridge_Codefest_Alco-Lock.ino
+++ Oakridge_Codefest_Alco-Lock.ino
@@ -8,11 +8,14 @@
 #define IR_L !digitalRead(10)
 #define IR_M !digitalRead(4)
 #define IR_R !digitalRead(2)
+int Echo = A4;  
+int Trig = A5; 
+int middleDistance = 0;
 
 void setup() {
   // put your setup code here, to run once:
- pinMode(A4,OUTPUT);
- pinMode(A5,INPUT);
+ pinMode(Echo,INPUT);
+ pinMode(Trig,OUTPUT);
  pinMode(10,INPUT);
  pinMode(4,INPUT);
  pinMode(2,INPUT);
@@ -43,8 +46,38 @@ void back(){
  digitalWrite(M3,LOW);
  digitalWrite(M4,HIGH);
 }
-
+void left(){
+ Serial.println("foward");
+ analogWrite(ENA,carspeed);
+ analogWrite(ENB,carspeed);
+ digitalWrite(M1,HIGH);
+ digitalWrite(M2,LOW);
+ digitalWrite(M3,HIGH);
+ digitalWrite(M4,LOW);
+}
+void right(){
+ Serial.println("back");
+ analogWrite(ENA,carspeed);
+ analogWrite(ENB,carspeed);
+ digitalWrite(M1,LOW);
+ digitalWrite(M2,HIGH);
+ digitalWrite(M3,LOW);
+ digitalWrite(M4,HIGH);
+}
+void stop(){
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+  Serial.println("Stop");
+}
 void loop() {
   // put your main code here, to run repeatedly:
+ int pol=analogRead(A0);
+ Serial.println(pol);
+if (pol>=275){
+  if(IR_M==LOW){
+    foward();
+  }
 
+  
+}
 }
